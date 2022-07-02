7311fb759ca3571100f5dd1e97628017c9c42c40
MichaelChristianChandra <c14200013@john.petra.ac.id>
2022-07-03 04:20:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -433,7 +433,7 @@ class Enemy(Characters):
     selfDmgReduc = self.inventory.seeEquippedArmor().getDetails()[2]
 
     playerHP = player.hp
-    self.inventory.seeEquippedWeapon()
+    self.inventory.seeEquippedWeapon(self.inventory.equippedWeapon)
     playerWeapon = player.inventory.seeEquippedWeapon()
     playerArmor = player.inventory.seeEquippedArmor()
 
